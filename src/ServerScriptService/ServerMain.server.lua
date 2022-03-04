92400bca383756cf1b2c0525a58acb6c01958202
DataEncoded <dataencoded@gmail.com>
2022-03-04 13:18:51 -0600

    [chore] Add Add(Services/Controllers)Deep to client and server, then load components

--- src/ServerScriptService/ServerMain.server.lua
+++ src/ServerScriptService/ServerMain.server.lua
@@ -1,7 +1,11 @@
 local Knit = require(game:GetService("ReplicatedStorage").Packages.knit)
+local Loader = require(game:GetService("ReplicatedStorage").Packages.loader)
+
+Knit.AddServicesDeep(script.Parent.Services)
 
 Knit.Start()
 	:andThen(function()
 		print("[ KNIT ] SERVER STARTED")
+		Loader.LoadDescendants(script.Parent.Components)
 	end)
 	:catch(warn)
--- src/StarterPlayer/StarterPlayerScripts/ClientMain.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/ClientMain.client.lua
@@ -1,7 +1,13 @@
 local Knit = require(game:GetService("ReplicatedStorage").Packages.knit)
+local Loader = require(game:GetService("ReplicatedStorage").Packages.loader)
 
+script.Parent:WaitForChild("Controller")
+Knit.AddControllersDeep(script.Parent.Controllers)
+
+--Knit start, print status and then load components
 Knit.Start()
 	:andThen(function()
 		print("[ KNIT ] CLIENT STARTED")
+		Loader.LoadDescendants(script.Parent.Components)
 	end)
 	:catch(warn)
