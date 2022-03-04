dd9eeaf378a5573ac1aa19524d95807059d069f1
DataEncoded <dataencoded@gmail.com>
2022-03-04 15:13:43 -0600

    [bugfix] Fix incorrect path given after changing knit organizational controller location

--- src/StarterPlayer/StarterPlayerScripts/ClientMain.client.lua
+++ src/StarterPlayer/StarterPlayerScripts/ClientMain.client.lua
@@ -2,12 +2,12 @@ local Knit = require(game:GetService("ReplicatedStorage").Packages.knit)
 local Loader = require(game:GetService("ReplicatedStorage").Packages.loader)
 
 script.Parent:WaitForChild("Controller")
-Knit.AddControllersDeep(script.Parent.Controllers)
+Knit.AddControllersDeep(game:GetService("ReplicatedStorage").Controllers)
 
 --Knit start, print status and then load components
 Knit.Start()
 	:andThen(function()
 		print("[ KNIT ] CLIENT STARTED")
-		Loader.LoadDescendants(script.Parent.Components)
+		Loader.LoadDescendants(game:GetService("ReplicatedStorage").Components)
 	end)
 	:catch(warn)
