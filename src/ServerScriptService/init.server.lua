4ff74c3adc7b132d024f2e1c92f3b9e21cfae40f
GummyScript <joshmuellerps@gmail.com>
2022-03-04 11:32:24 -0600

    Server/Client Fix

--- src/ServerScriptService/init.server.lua
+++
@@ -1,7 +0,0 @@
-local Knit = require(game:GetService("ReplicatedStorage").Packages.knit)
-
-Knit.Start()
-	:andThen(function()
-		print("[ KNIT ] SERVER STARTED")
-	end)
-	:catch(warn)
--- src/StarterPlayerScripts/init.client.lua
+++
@@ -1,7 +0,0 @@
-local Knit = require(game:GetService("ReplicatedStorage").Packages.knit)
-
-Knit.Start()
-	:andThen(function()
-		print("[ KNIT ] CLIENT STARTED")
-	end)
-	:catch(warn)
