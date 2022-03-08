23b2ca7d94231b0c1e2bf0ebf653586e2eaa6297
GummyScript <52335565+GummyScript@users.noreply.github.com>
2022-03-07 23:00:44 -0600

    Camera Control (#3)
    * [feat] Camera functionality
    
    * [feat] Added Controller (Right-Analog) rotation functionality
    
    * [fix] Checks to see if Gamepad is Enabled
    
    * [tweak] Speed up controller input rotation
    
    * [tweak] Further speed up of Controller input rotation

---
+++ src/ReplicatedStorage/Controllers/CameraController.lua
@@ -0,0 +1,50 @@
+local Knit = require(game:GetService("ReplicatedStorage").Packages.knit)
+
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+
+local CameraController = Knit.CreateController({
+	Name = "CameraController",
+})
+
+local player
+local camera = workspace.CurrentCamera
+local cameraOffset = CFrame.new(30, 50, 30) -- This will change based off what ship they have equipped
+local totalSpinY = 0
+
+local function Bind()
+	local character = player.Character
+	local root = character and character.PrimaryPart
+
+	if not root then
+		return
+	end
+
+	local delta = UserInputService:GetMouseDelta()
+	local deltaX = delta.X * 0.5
+
+	if UserInputService.GamepadEnabled then
+		local gamepadState = UserInputService:GetGamepadState(Enum.UserInputType.Gamepad1)
+		for _, state in pairs(gamepadState) do
+			if state.KeyCode.Name == "Thumbstick2" then
+				delta = state.Position
+				deltaX = state.Position.X * 1.5
+				break
+			end
+		end
+	end
+
+	totalSpinY += deltaX
+	local rootCFrame = CFrame.new(root.Position) * CFrame.Angles(0, math.rad(totalSpinY), 0)
+	local cameraPosition = (rootCFrame * cameraOffset).Position
+	camera.CFrame = CFrame.lookAt(cameraPosition, rootCFrame.Position)
+end
+
+function CameraController:KnitStart() end
+
+function CameraController:KnitInit()
+	player = game.Players.LocalPlayer
+	RunService:BindToRenderStep("IsometricCamera", Enum.RenderPriority.Camera.Value + 1, Bind)
+end
+
+return CameraController
