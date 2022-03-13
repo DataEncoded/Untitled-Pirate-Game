f6e80d76df79b0cb5a9e00806585429e0d9603bc
DataEncoded <dataencoded@gmail.com>
2022-03-12 21:40:15 -0600

    [refactor] Change selene linting problems x3

--- src/ReplicatedStorage/Controllers/CameraController.lua
+++ src/ReplicatedStorage/Controllers/CameraController.lua
@@ -39,7 +39,7 @@ local function Bind(partToBind)
 end
 
 function CameraController:BindToPart(partToBind)
-	RunService:BindToRenderStep("IsometricCamera", Enum.RenderPriority.Camera.Value + 1, function(dt)
+	RunService:BindToRenderStep("IsometricCamera", Enum.RenderPriority.Camera.Value + 1, function()
 		Bind(partToBind)
 	end)
 end
