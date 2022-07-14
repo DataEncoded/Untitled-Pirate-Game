87868e7803a0ba3c9f4de5a9e9f6f6ea60e20706
DataEncoded <dataencoded@gmail.com>
2022-07-14 15:16:45 -0500

    [format] Run stylua on the PlayerShipCannonService

--- src/ServerScriptService/Services/ShipServices/PlayerShipCannonService.lua
+++ src/ServerScriptService/Services/ShipServices/PlayerShipCannonService.lua
@@ -12,7 +12,6 @@ local QuickFunctions = require(game.ReplicatedStorage.Modules.QuickFunctions)
 local Components = ReplicatedStorage.Components
 local shipComponent
 
-
 local PlayerShipCannonService = Knit.CreateService({
 	Name = "PlayerShipCannonService",
 	Client = {
@@ -22,73 +21,64 @@ local PlayerShipCannonService = Knit.CreateService({
 })
 
 local function hitDetection(playerShip, startPos, endPos)
+	local params = OverlapParams.new()
+	params.FilterType = Enum.RaycastFilterType.Blacklist
+	params.FilterDescendantsInstances = playerShip
 
-    local params = OverlapParams.new()
-    params.FilterType = Enum.RaycastFilterType.Blacklist
-    params.FilterDescendantsInstances = playerShip
+	local function loopFunction(quadPosition)
+		local result = QuickFunctions.hitDetection(quadPosition, params)
+		if result then
+			local ship = shipComponent:FromInstance(result)
 
+			if ship then
+				--TODO: CANNON DAMAGE CHANGING DEPENDING ON CUSTOMIZATION
+				ship:takeDamage(5)
+			end
 
-    local function loopFunction(quadPosition)
-        local result = QuickFunctions.hitDetection(quadPosition, params)
-        if result then
-            
-            local ship = shipComponent:FromInstance(result)
+			return true
+		end
+	end
 
-            if ship then
-                --TODO: CANNON DAMAGE CHANGING DEPENDING ON CUSTOMIZATION
-                ship:takeDamage(5)
-            end
-
-            return true
-        end
-    end
-
-
-    QuickFunctions.loopOverQuad(startPos, endPos, nil, loopFunction)
-    
+	QuickFunctions.loopOverQuad(startPos, endPos, nil, loopFunction)
 end
 
 function PlayerShipCannonService:KnitStart()
+	local cooldown = {}
 
-    local cooldown = {}
+	--Wait and listen for cancel, and when cooldown is active add player to cooldown table
+	local function playerFireCooldown(player: Player, waitTime: number)
+		return Promise.new(function(resolve, _, cancel)
+			cooldown[player] = true
 
-    --Wait and listen for cancel, and when cooldown is active add player to cooldown table
-    local function playerFireCooldown(player: Player, waitTime: number)
-        return Promise.new(function(resolve, _, cancel)
-            cooldown[player] = true
-            
-            for _ = 0, waitTime, 0.1 do
-                task.wait(0.1)
-                if cancel() then 
-                    break 
-                end
-            end
+			for _ = 0, waitTime, 0.1 do
+				task.wait(0.1)
+				if cancel() then
+					break
+				end
+			end
 
-            cooldown[player] = nil
-            resolve()
-        end)
-    end
+			cooldown[player] = nil
+			resolve()
+		end)
+	end
 
-    self.Client.Fire:Connect(function(player, position)
-        if position then
-            --TODO: Check that the cannon is within the desired angle
-            local playerShips = QuickFunctions.returnTaggedAttribute("PlayerShip", "UserId", player.UserId)
+	self.Client.Fire:Connect(function(player, position)
+		if position then
+			--TODO: Check that the cannon is within the desired angle
+			local playerShips = QuickFunctions.returnTaggedAttribute("PlayerShip", "UserId", player.UserId)
 
-            if #playerShips == 1 and not cooldown[player] then
-                self.Client.Fire:FireAll(playerShips[1]:GetPivot().Position, position, playerShips[1])
+			if #playerShips == 1 and not cooldown[player] then
+				self.Client.Fire:FireAll(playerShips[1]:GetPivot().Position, position, playerShips[1])
 
-                --TODO: Add specific time for cannon reload
-                playerFireCooldown(player, 3)
+				--TODO: Add specific time for cannon reload
+				playerFireCooldown(player, 3)
 
-                hitDetection(playerShips, playerShips[1]:GetPivot().Position, position)
-            end
+				hitDetection(playerShips, playerShips[1]:GetPivot().Position, position)
+			end
+		end
+	end)
 
-        end
-    end)
-    
-    shipComponent = require(Components.Ship)
-
+	shipComponent = require(Components.Ship)
 end
-
 
 return PlayerShipCannonService
